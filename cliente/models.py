7354afa3fc9fe002b5472a018a3f5a3a69c559b1
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-08-24 19:43:31 -0300

    Update models.py

--- cliente/models.py
+++ cliente/models.py
@@ -4,7 +4,7 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=255)
     cpf = models.CharField(max_length=14, unique=True)
     telefone = models.CharField(max_length=20)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.nome} (CPF: {self.cpf})"
