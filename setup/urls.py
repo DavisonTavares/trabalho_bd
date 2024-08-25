67e5f7343ad3ebbe091d8fc00a49b38318853b66
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-08-25 12:45:50 -0300

    ajustes html

--- setup/urls.py
+++ setup/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('lista_clientes', listar_cliente.as_view(),name="lista_clientes"),
     path('', listar_cliente.as_view(),name="home"),
     path('cadastrar_cliente', cadastrar_cliente.as_view(), name='cadastrar_cliente'),
+    path('editar_cliente/<int:pk>',editar_cliente.as_view(),name="editar_cliente"),
+    path('deletar_cliente/<int:pk>',deletar_cliente.as_view(),name="deletar_cliente"),
     path('cadastrar_poduto/',cadastrar_produto,name="cadastrar_produto"),
     path('editar_produto/<uuid:produto_id>/', editar_produto, name='editar_produto'),
     path('lista_produtos/',listar_produto,name="lista_produtos"),
-    path('editar_cliente/<int:pk>',editar_cliente.as_view(),name="editar_cliente"),
-    path('deletar_cliente/<int:pk>',deletar_cliente.as_view(),name="deletar_cliente"),
 ]
