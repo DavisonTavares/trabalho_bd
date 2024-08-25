f2f974bda5c35d998953e0129e22b10d957dcf05
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-08-25 18:49:21 -0300

    alterações models cliente

--- setup/urls.py
+++ setup/urls.py
@@ -1,15 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path
-from cliente.views import cadastrar_cliente,listar_cliente,editar_cliente,deletar_cliente
+from cliente.views import cadastrar_cliente,listar_cliente,editar_cliente,deletar_cliente,gerar_relatorio_cliente
 from produto.views import cadastrar_produto, editar_produto, listar_produto, deletar_produto, gerar_relatorio
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lista_clientes', listar_cliente.as_view(),name="lista_clientes"),
+    path('lista_clientes/', listar_cliente.as_view(),name="lista_clientes"),
     path('', listar_cliente.as_view(),name="home"),
-    path('cadastrar_cliente', cadastrar_cliente.as_view(), name='cadastrar_cliente'),
-    path('editar_cliente/<int:pk>',editar_cliente.as_view(),name="editar_cliente"),
-    path('deletar_cliente/<int:pk>',deletar_cliente.as_view(),name="deletar_cliente"),
+    path('cadastrar_cliente/', cadastrar_cliente.as_view(), name='cadastrar_cliente'),
+    path('editar_cliente/<uuid:cliente_id>/',editar_cliente.as_view(),name="editar_cliente"),
+    path('deletar_cliente/<uuid:cliente_id>/',deletar_cliente.as_view(),name="deletar_cliente"),
+    path('gerar_relatorio_cliente/', gerar_relatorio_cliente, name='gerar_relatorio_cliente'),
     path('cadastrar_poduto/',cadastrar_produto,name="cadastrar_produto"),
     path('editar_produto/<uuid:produto_id>/', editar_produto, name='editar_produto'),
     path('deletar_produto/<uuid:produto_id>/', deletar_produto, name='deletar_produto'),
